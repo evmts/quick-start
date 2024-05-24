6cf495c4ca5bfceeb1c21a0cf31f206458241791
William Cory <williamcory@Williams-MacBook-Pro.local>
2024-05-24 10:51:22 -0700

    feat: Add function that displays account balance

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,25 @@ const memoryClient = createMemoryClient({
 	},
 });
 
+// addresses and abis must be as const for tevm types
+const address = `0x${"0420".repeat(10)}` as const;
+async function updateAccounts() {
+	// we are setting throwOnFail to false because we expect this to throw an error from the account not existing
+	const account = await memoryClient.tevmGetAccount({
+		address,
+		throwOnFail: false,
+	});
+	if (account.errors) {
+		// this will error
+		console.error("Unable to get account", account.errors);
+		return;
+	}
+	console.log(account); // console log the account to get familiar with what properties are on it
+	document.querySelector("#address")!.innerHTML = address;
+	document.querySelector("#nonce")!.innerHTML = String(account.nonce);
+	document.querySelector("#balance")!.innerHTML = String(account.balance);
+}
+
 async function runApp() {
 	app.innerHTML = `<div id="status">initializing...</div>
 <div id="blocknumber"></div>
@@ -31,6 +50,10 @@ async function runApp() {
 	const blockNumber = await memoryClient.getBlockNumber();
 	document.querySelector("#blocknumber")!.innerHTML =
 		`ForkBlock: ${blockNumber}`;
+
+	status.innerHTML = "Updating accounts...";
+	await updateAccounts();
+
 	status.innerHTML = "Done";
 }
 
