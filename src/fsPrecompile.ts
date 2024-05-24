42953c13cfdfdfe898ab3ed8afdf14b5fb719671
William Cory <williamcory@Williams-MacBook-Pro.local>
2024-05-24 12:17:34 -0700

    feat: Add fsPrecompile implementation

---
+++ src/fsPrecompile.ts
@@ -0,0 +1,21 @@
+import fs from "node:fs/promises";
+import { defineCall, definePrecompile } from "tevm";
+import { Fs } from "../contracts/Fs.sol";
+
+const contract = Fs.withAddress("0xf2f2f2f2f2f2f2f2f2f2f2f2f2f2f2f2f2f2f2f2");
+
+export const fsPrecompile = definePrecompile({
+	contract,
+	call: defineCall(Fs.abi, {
+		readFile: async ({ args }) => {
+			return {
+				returnValue: await fs.readFile(...args, "utf8"),
+				executionGasUsed: 0n,
+			};
+		},
+		writeFile: async ({ args }) => {
+			await fs.writeFile(...args);
+			return { returnValue: true, executionGasUsed: 0n };
+		},
+	}),
+});
