521cd1775c9363c2060a5e8ffecab1be3156b996
William Cory <williamcory@Williams-MacBook-Pro.local>
2024-05-24 11:25:22 -0700

    await tevm.ready()

--- src/main.ts
+++ src/main.ts
@@ -86,6 +86,10 @@ async function runApp() {
 
 	const status = app.querySelector("#status")!;
 
+	status.innerHTML = "Initializing memory client...";
+	// this isn't necessary to explicitly await readyness as other actions will wait for it implicitly
+	await memoryClient.tevmReady();
+
 	status.innerHTML = "Fetching block number...";
 
 	const blockNumber = await memoryClient.getBlockNumber();
