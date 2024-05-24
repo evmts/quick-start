9b690b290e665991ee71dc06ab80cc8ac39ab6ee
William Cory <williamcory@Williams-MacBook-Pro.local>
2024-05-24 11:45:19 -0700

    feat: Update vite.config.js with bundler
    update vite.config with bundler fix

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,21 @@
-
-import { defineConfig } from "vite"
-import { nodePolyfills } from "vite-plugin-node-polyfills"
+import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+import { vitePluginTevm } from "@tevm/bundler/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  define: {
-    global: "globalThis",
-  },
-  plugins: [
-    nodePolyfills({
-      include: ["stream"],
-      globals: {
-        process: true,
-        Buffer: true,
-        global: true,
-      },
-    }),
-  ],
-})
-
+	define: {
+		global: "globalThis",
+	},
+	plugins: [
+		vitePluginTevm(),
+		nodePolyfills({
+			include: ["stream"],
+			globals: {
+				process: true,
+				Buffer: true,
+				global: true,
+			},
+		}),
+	],
+});
