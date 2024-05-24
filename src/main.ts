14cc9ae57fdec784f9d779010b91df9b6935055a
William Cory <williamcory@Williams-MacBook-Pro.local>
2024-05-24 10:44:24 -0700

    feat: Create a memory client

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,31 @@
+import { createMemoryClient, http } from "tevm";
+import { optimism } from "tevm/common";
 
 const app = document.querySelector("#app") as Element;
-app.innerHTML = `<div>Hello Tevm</div>`;
 
+const memoryClient = createMemoryClient({
+	common: optimism,
+	fork: {
+		// @warning we may face throttling using the public endpoint
+		// In production apps consider using `loadBalance` and `rateLimit` transports
+		transport: http("https://mainnet.optimism.io")({}),
+	},
+});
+
+async function runApp() {
+	app.innerHTML = `<div id="status">initializing...</div>
+<div id="blocknumber"></div>
+`;
+	const status = app.querySelector("#status")!;
+
+	status.innerHTML = "Fetching block number...";
+
+	document.querySelector("#blocknumber")!.innerHTML = `Fetching block number next step. For now let's check out which methods are on memory client:
+<ul>
+  ${Object.keys(memoryClient).map((key) => `<li>${key}</li>`)}
+</ul>`;
+
+	status.innerHTML = "Done";
+}
+
+runApp();
